96eb49c803499d4398dda131ad5ccfb8cef1af59
Guilherme Ferreira <101301922+guilherme90rr@users.noreply.github.com>
2022-08-18 15:18:39 -0300

    Add files via upload
    Cálculo desconto de 20%

---
+++ porcentagem.py
@@ -0,0 +1,16 @@
+# Vamos pedir o valor originl do produto 
+valor_original = float(input("Valor original: R$ "))
+
+# Desconto ganh  de 20%
+valor_descontado = valor_original * 0.2
+
+# Valor com desconto incluso
+novo_valor = valor_original * 0.80
+
+# Vamor pedir o valor original do produto
+#valor_original = float(input("Valor orignal: R$ "))
+
+# Resultado final
+print("Valor original: R$", valor_original)
+print("Desconto ganho: R$", valor_original * 0.2)
+print("Valor com desconto: R$", valor_original * 0.8)
